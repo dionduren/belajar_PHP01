5198e936fde58bb1212fc19a18494f52c0c388ec
Dion Alamsah <dion.alamsah@PUPUK-INDONESIA.local>
2022-07-01 09:50:36 +0800

    Menambahkan keterangan command git di php pertama

---
+++ Belajar_Git/latihan1.php
@@ -0,0 +1,74 @@
+<?php
+// Pengulangan
+// for
+// while
+// do ... while
+// foreach => Array
+
+
+
+// Pengkondisian
+// If, Else If, Else, 
+// Ternary  
+// switch
+
+echo "Halo <br/> <br>";
+
+
+// git status - check file status
+// git add . - tambahkan semua file baru
+?>
+
+<?php
+
+function coba()
+{
+    $nama = "Dion";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <table>
+        <tr>
+            <Td>
+                Username
+            </Td>
+            <Td>
+                :
+            </Td>
+            <Td>
+                <label>
+                    <input type="text" name="username">
+                </label>
+            </Td>
+        </tr>
+
+        <tr>
+            <Td>
+                Password
+            </Td>
+            <Td>
+                :
+            </Td>
+            <Td>
+                <label>
+                    <input type="password" name="password">
+                </label>
+            </Td>
+        </tr>
+    </table>
+</body>
+
+</html>
